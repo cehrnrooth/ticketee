d8a668753e92bd5249581b4809d7fc806075d3cc
Carl <carl.ehrnrooth@gmail.com>
2013-01-07 10:13:34 -0800

    CRUD Features

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,8 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
   
   def index
+    @projects = Project.find(:all)
   end
   
   def new
@@ -9,13 +11,43 @@ class ProjectsController < ApplicationController
   
   def create
     @project = Project.new(params[:project])
-    @project.save
-    flash[:notice] = "Project has been created."
-    redirect_to @project
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash[:alert] = "Project could not be created"
+      render :action => 'new'
+    end
   end
   
   def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project could not be updated."
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @project.destroy
+    flash[:notice] = "Project has been deleted"
+    redirect_to projects_path
+  end
+
+private
+  def find_project
     @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
   end
   
 end
